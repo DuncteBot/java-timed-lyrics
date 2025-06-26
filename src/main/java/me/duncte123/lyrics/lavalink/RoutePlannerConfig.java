5248346e5359321af9cbd351ba683f8046157048
infnibor <156368035+infnibor@users.noreply.github.com>
2025-06-26 16:25:11 +0200

    Fix issue (https://github.com/DuncteBot/java-timed-lyrics/issues/11)

---
+++ src/main/java/me/duncte123/lyrics/lavalink/RoutePlannerConfig.java
@@ -0,0 +1,17 @@
+import com.sedmelluq.lava.extensions.youtuberotator.planner.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+public class RoutePlannerConfig {
+    @Bean
+    public AbstractRoutePlanner routePlanner() {
+        return new RotatingNanoIpRoutePlanner(
+            Collections.emptyList(),
+            addr -> false
+        );
+    }
+}
